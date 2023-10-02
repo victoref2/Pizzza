5b255553f5f7edfb8f2bb9f61e30aa1518a28dd7
victoref <victoref3@gmail.com>
2023-10-02 08:16:34 +0200

    Add project files.

---
+++ Pizzza app/Custom pizza.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pizzza_app
+{
+    /// <summary>
+    /// Interaction logic for Custom_pizza.xaml
+    /// </summary>
+    public partial class Custom_pizza : Window
+    {
+        // Add properties to store customization options, e.g., selectedToppings
+
+        private List<Toppings> selectedToppings;
+
+        public Custom_pizza(List<Toppings> selectedToppings)
+        {
+            InitializeComponent();
+            this.selectedToppings = selectedToppings;
+
+            // You can now use 'this.selectedToppings' in this window to access the selected pizza's toppings.
+        }
+
+        // Handle the customization and create the custom pizza
+        private void CustomizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new Pizza_Menu instance with selected customization
+            Pizza_Menu customPizza = new Pizza_Menu
+            {
+              //  Number = pizzaItems.Count + 1, // Assign a unique number
+               // Name = originalPizzaName + " +", // Append a "+" to the original name
+                //Price = CalculateCustomPizzaPrice(), // Calculate the price based on selections
+                //Description = "Custom " + originalPizzaName, // Customize the description
+                //Toppings = selectedToppings // Add selected toppings
+            };
+
+            // Add the custom pizza to the pizzaItems list in the main window
+            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            if (mainWindow != null)
+            {
+                mainWindow.AddCustomPizzaToMenu(customPizza);
+            }
+
+            this.Close(); // Close the custom pizza window
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+             
+            
+
+        }
+    }
+}
---
+++ Pizzza app/MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pizzza_app
+{
+    public class CartItem
+    {
+        public MenuItem MenuItem { get; }
+        public int Quantity { get; set; }
+
+        public CartItem(MenuItem menuItem, int quantity = 1)
+        {
+            MenuItem = menuItem;
+            Quantity = quantity;
+        }
+    }
+
+    public abstract class MenuItem
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Pizza_Menu : MenuItem
+    {
+        public List<Toppings> Toppings { get; set; }
+        public string Description { get; set; }
+        public string ToppingsString
+        {
+            get
+            {
+                if (Toppings != null)
+                {
+                    return string.Join(", ", Toppings.Select(t => t.Topping));
+                }
+                return string.Empty;
+            }
+        }
+    }
+
+
+    public class Toppings : Pizza_Menu
+    {
+        
+        public string Topping { get; set; }
+        public int ToppingPrice { get; set; }
+
+        
+    }
+
+    public class Drink_Menu : MenuItem
+    {
+        // Additional properties specific to drinks can be added here
+    }
+
+    public class Extra_Menu : MenuItem
+    {
+        // Additional properties specific to extras can be added here
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Pizza_Menu> pizzaItems = new List<Pizza_Menu>();
+        private List<Drink_Menu> drinkItems = new List<Drink_Menu>();
+        private List<Extra_Menu> extraItems = new List<Extra_Menu>();
+        public List<Toppings> ToppingsList = new List<Toppings>();
+
+        public ObservableCollection<CartItem> cartItems = new ObservableCollection<CartItem>();
+
+        List<Toppings> availableToppings = new List<Toppings>
+        {
+        new Toppings { Topping = "Pepperoni", ToppingPrice = 0 },//0
+        new Toppings { Topping = "Mushrooms", ToppingPrice = 0 },//1
+        new Toppings { Topping = "Sausage", ToppingPrice = 0 },//2
+        new Toppings { Topping = "Gouda", ToppingPrice = 0 },//3
+        new Toppings { Topping = "Cheddar", ToppingPrice = 0 },//4
+        new Toppings { Topping = "Cheese", ToppingPrice = 0 },//5
+        new Toppings { Topping = "Basil", ToppingPrice = 0 },//6
+        new Toppings { Topping = "Mozzarela", ToppingPrice = 0 },//7
+        new Toppings { Topping = "Tomato", ToppingPrice = 0 },//8
+        new Toppings { Topping = "BBQ Sauce", ToppingPrice = 0 },//9
+        new Toppings { Topping = "Crab", ToppingPrice = 0 },//10
+        new Toppings { Topping = "Onions", ToppingPrice = 0 },//11
+        new Toppings { Topping = "Red Onion", ToppingPrice = 0 },//12
+        new Toppings { Topping =  "Bacon", ToppingPrice = 0 },//13
+        new Toppings { Topping =  "Chicken", ToppingPrice = 0 },//14
+        new Toppings { Topping =  "Pork", ToppingPrice = 0 },//15
+        new Toppings { Topping = "ham", ToppingPrice = 0},//16
+        new Toppings { Topping = "pineapple", ToppingPrice = 0 },//17
+        new Toppings { Topping = "Chili and more", ToppingPrice = 0 }, //18
+        new Toppings { Topping = "Tuna", ToppingPrice= 0 },//19
+        // Add more toppings as needed
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pizzaItems.Add(new Pizza_Menu { Number = 1, Name = "Pepperoni Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 69.99M, Description = "Classic Pepperoni Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 2, Name = "Margherita Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[7] }, Price = 60M, Description = "Traditional Margherita Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 3, Name = "Vegetarian Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[1], availableToppings[8] }, Price = 100M, Description = "Veggie-Loaded Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 4, Name = "Meat Lovers Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0], availableToppings[15], availableToppings[16], availableToppings[2] }, Price = 100M, Description = "For the Meat Lovers" });
+            pizzaItems.Add(new Pizza_Menu { Number = 5, Name = "BBQ Chicken Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[14], availableToppings[9] }, Price = 80M, Description = "Tangy BBQ Chicken Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 6, Name = "Supreme Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 80M, Description = "Loaded Supreme Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 7, Name = "Hawaiian Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 79.99M, Description = "Sweet and Savory Hawaiian Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 8, Name = "Four Cheese Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 80m, Description = "A Cheesy Delight" });
+            pizzaItems.Add(new Pizza_Menu { Number = 9, Name = "Mushroom Truffle Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 80M, Description = "Elegant Mushroom and Truffle Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 10, Name = "Sausage and Pepper Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 75M, Description = "Classic Combo" });
+            pizzaItems.Add(new Pizza_Menu { Number = 11, Name = "Buffalo Chicken Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 80M, Description = "Spicy Buffalo Chicken Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 12, Name = "Pesto Veggie Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 70M, Description = "Fresh and Flavorful" });
+            pizzaItems.Add(new Pizza_Menu { Number = 13, Name = "Barbecue Bacon Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 70M, Description = "Sweet and Savory Combo" });
+            pizzaItems.Add(new Pizza_Menu { Number = 14, Name = "Artichoke and Olive Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 70M, Description = "Mediterranean Flavors" });
+            pizzaItems.Add(new Pizza_Menu { Number = 15, Name = "Spinach and Feta Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 65M, Description = "Greek Inspired Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 16, Name = "Bacon and Egg Breakfast Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 90M, Description = "Morning Delight" });
+            pizzaItems.Add(new Pizza_Menu { Number = 17, Name = "BBQ Pork Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 85M, Description = "Hearty BBQ Pork Pizza" });
+            pizzaItems.Add(new Pizza_Menu { Number = 18, Name = "Pepper and Onion Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[0] }, Price = 80M, Description = "Simple and Delicious" });
+            pizzaItems.Add(new Pizza_Menu { Number = 19, Name = "Seafood Supreme Pizza", Toppings = new List<Toppings> { availableToppings[5], availableToppings[10], availableToppings[19] }, Price = 90M, Description = "Seafood Lover's Delight" });
+            pizzaItems.Add(new Pizza_Menu { Number = 20, Name = "DEMO-MAN", Toppings =new List<Toppings> { availableToppings[18] }, Price = 110m, Description ="if you want to die"});
+            drinkItems.Add(new Drink_Menu { Number = 1, Name = "Soda", Price = 12M });
+            drinkItems.Add(new Drink_Menu { Number = 2, Name = "Lemonade", Price = 5.99M });
+            drinkItems.Add(new Drink_Menu { Number = 3, Name = "Iced Tea", Price = 6.49M });
+            drinkItems.Add(new Drink_Menu { Number = 4, Name = "Orange Juice", Price = 12.99M });
+            drinkItems.Add(new Drink_Menu { Number = 5, Name = "Coca-Cola", Price = 14.99M });
+            drinkItems.Add(new Drink_Menu { Number = 6, Name = "Sprite", Price = 14.99M });
+            extraItems.Add(new Extra_Menu { Number = 1, Name = "Garlic Bread", Price = 9.99M });
+            extraItems.Add(new Extra_Menu { Number = 2, Name = "Pomfritter small", Price = 14.99M });
+            extraItems.Add(new Extra_Menu { Number = 3, Name = "Pomfirtter Stor", Price = 29.99M });
+            extraItems.Add(new Extra_Menu { Number = 4, Name = "Mozzarella Sticks", Price = 9.99M });
+            extraItems.Add(new Extra_Menu { Number = 5, Name = "Salad", Price = 10.49M });
+            extraItems.Add(new Extra_Menu { Number = 6, Name = "Chicken Wings", Price = 19.99M });
+            extraItems.Add(new Extra_Menu { Number = 7, Name = "Onion Rings", Price = 14.99M });
+            ComboBoxItem pizzaType = new ComboBoxItem { Content = "Select Menu Type" };
+            ComboBoxItem pizzaItem = new ComboBoxItem { Content = "Pizza" };
+            ComboBoxItem drinkItem = new ComboBoxItem { Content = "Drink" };
+            ComboBoxItem extraItem = new ComboBoxItem { Content = "Extra" };
+            Selection.Items.Add(pizzaType);
+            Selection.Items.Add(pizzaItem);
+            Selection.Items.Add(drinkItem);
+            Selection.Items.Add(extraItem);
+
+
+            Selection.SelectedIndex = 0;
+            CartItemsGrid.ItemsSource = cartItems;
+        }
+
+
+        public void AddCustomPizzaToMenu(Pizza_Menu customPizza)
+        {
+            // Add the custom pizza to the pizzaItems list
+            pizzaItems.Add(customPizza);
+
+            // Refresh the menu selection
+            MenuSelect.Items.Refresh();
+        }
+
+        public void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selectedType = Selection.SelectedItem as ComboBoxItem;
+            string menuType = selectedType.Content.ToString();
+
+            if (menuType == "Pizza" && MenuSelect.ItemsSource != pizzaItems)
+            {
+                MenuSelect.ItemsSource = pizzaItems;
+
+                // Define the columns for Pizza menu with Toppings and Description
+                MenuSelect.Columns.Clear();
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Number", Binding = new Binding("Number") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Name", Binding = new Binding("Name") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Toppings", Binding = new Binding("ToppingsString") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Description", Binding = new Binding("Description") });
+            }
+            else if (menuType == "Drink" && MenuSelect.ItemsSource != drinkItems)
+            {
+                MenuSelect.ItemsSource = drinkItems;
+
+                // Define the columns for Drink menu
+                MenuSelect.Columns.Clear();
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Number", Binding = new Binding("Number") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Name", Binding = new Binding("Name") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
+            }
+            else if (menuType == "Extra" && MenuSelect.ItemsSource != extraItems)
+            {
+                MenuSelect.ItemsSource = extraItems;
+
+                // Define the columns for Extra menu
+                MenuSelect.Columns.Clear();
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Number", Binding = new Binding("Number") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Name", Binding = new Binding("Name") });
+                MenuSelect.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+         {
+            if (CartItemsGrid.SelectedItem is CartItem selectedItem)
+            {
+                cartItems.Remove(selectedItem);
+                CalculateTotalPrice(); // Update total price after item is removed
+                                       // Optionally, update the cart display or any related UI elements here
+            }
+        }
+
+
+        
+        private decimal CalculateTotalPrice()
+        {
+
+            decimal totalPrice = 0;
+
+            foreach (CartItem cartItem in cartItems)
+            {
+                totalPrice += cartItem.MenuItem.Price * cartItem.Quantity;
+            }
+
+            return totalPrice;
+        }
+
+
+
+
+        public void Buy_check_out_Click(object sender, RoutedEventArgs e)
+        {
+            List<CartItem> ShopCartItems = cartItems.ToList();
+            decimal fullPrice = CalculateTotalPrice();
+            Checkout_page checkoutPage = new Checkout_page(ShopCartItems, fullPrice);
+            checkoutPage.Show();
+        }
+
+        private void ADD_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (MenuSelect.SelectedItem != null && MenuSelect.SelectedItem is MenuItem selectedItem)
+    {
+        CartItem cartItem = new CartItem(selectedItem); // Create a CartItem instance
+        cartItems.Add(cartItem); // Add the item to the cartItems collection
+        CartItemsGrid.Items.Refresh();
+        // Optionally, update the cart display or any related UI elements here
+    }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (MenuSelect.SelectedItem is Pizza_Menu selectedPizza)
+            {
+                // Create an instance of the Custom_pizza window
+                Custom_pizza customPizzaWindow = new Custom_pizza(selectedPizza.Toppings);
+
+                // Show the window
+                customPizzaWindow.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Please select a pizza from the menu.");
+            }
+        }
+    }
+}
